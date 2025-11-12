d890780b3bd81f45ebea376fc517b4cf8bc56ef1
marcelloniek <mniek@hotmail.com>
2025-11-12 18:28:08 -0300

    Prot├│tipo ViverBem Escola (mobile frame)

---
+++ git init.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "Protótipo ViverBem Escola (mobile frame)"
+# crie um repositório vazio no GitHub (pelo site) e depois:
+git branch -M main
+git remote add origin https://github.com/marcelloniek/viverbem-escola-prototipo.git
+git push -u origin main
